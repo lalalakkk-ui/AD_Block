8f7f10d004634ed46941f2ce5cf404937202df7c
lalalakkk-ui <g403418416@gmail.com>
2026-01-10 14:30:44 +0800

    删除 tieba_ae_identity.js

--- QX/JS/tieba_ae_identity.js
+++
@@ -1,9 +0,0 @@
-// tieba_ae_identity.js
-// Force Accept-Encoding to identity so server returns plain protobuf.
-
-let h = $request.headers || {};
-// 兼容大小写
-h["Accept-Encoding"] = "identity";
-h["accept-encoding"] = "identity";
-
-$done({ headers: h });
